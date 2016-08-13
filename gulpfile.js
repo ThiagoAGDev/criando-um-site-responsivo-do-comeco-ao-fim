0ee95361e50c8becedae7c9bc81ce8da5123e071
ThiagoAgDev <thiago@plug.ag>
2016-08-13 15:16:41 -0300

    Adicionado toda a estrutura sass e dependências que serão utilizadas no projeto

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 
 // task para o sass
-gulp.task('sass', function(){
-	return sass('sass/*.sass').pipe(gulp.dest('css'))
+gulp.task('sass', function() {
+    return gulp.src('sass/**/*.sass')
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('css'));
 });
 
 // task para watch
 gulp.task('watch', function(){
-	gulp.watch('sass/*.sass', ['sass']);
+	gulp.watch('sass/**/*.sass', ['sass']);
 });
 
 // task default gulp
