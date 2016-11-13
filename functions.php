75d86d8bd70e8e252bd28a4f633c826c4eacf9b2
ThiagoAgDev <thiago@plug.ag>
2016-11-13 13:14:18 -0200

    criado post_type para habilidades e custom fields

--- functions.php
+++ functions.php
@@ -11,3 +11,22 @@
 		);
 	}
 	add_action('init', 'register_my_menus');
+
+	// Meus posts types
+	function meus_posts_types() {
+		// Habilidades
+		register_post_type('habilidades',
+			array(
+				'labels'			=> array(
+					'name'			=> __('Habilidades'),
+					'singular_name' =>	__('Habilidade')
+					),
+				'public'			=> true,
+				'has_archive'		=> true,
+				'menu_icon'			=> 'dashicons-welcome-learn-more',
+				'supports'			=>	array('title', 'thumbnail', 'page-attributes'),
+				)
+
+		);
+	}
+	add_action('init', 'meus_posts_types');
--- includes/pages/sobre.php
+++ includes/pages/sobre.php
@@ -4,32 +4,38 @@
 				<p><strong>Lorem ipsum dolor sit amet</strong>, consectetur adipisicing elit. Aut in laborum, quos corrupti omnis ipsum reiciendis? Aspernatur, sequi eius! Nobis veniam eius autem dolores, doloribus deserunt est ex ea voluptatem.</p>
 			</div>
 
-			<ul class="habilidades">
-				<li class="habilidade-8">
+			<?php query_posts('post_type=habilidades&post_per_page=-1'); ?>
+<ul class="habilidades">
+				<?php if(have_posts()): ?>
+
+					<?php while(have_posts()): the_post(); ?>
+
+				<li class="habilidade-<?php the_field('nivel'); ?>">
 					<h2>
-						<i class="fa fa-html5"></i> <strong>HTML5</strong> // 80%
+						<style>
+							.<?php the_field('icone'); ?> {
+								<?php if(get_field('cor_do_icone')): ?>
+									color: <?php the_field('cor_do_icone'); ?>;
+								<?php else : ?>
+									color: #000;
+								<?php endif; ?>
+							}
+						</style>
+						<i class="fa <?php the_field('icone'); ?>"></i> <strong><?php the_title(); ?></strong> // <?php the_field('nivel') ?>0%
 						<div class="barra"><span></span></div>
 					</h2>
 				</li>
-				<li class="habilidade-5">
-					<h2>
-						<i class="fa fa-css3"></i> <strong>CSS3</strong> // 50%
-						<div class="barra"><span></span></div>
-					</h2>
-				</li>
-				<li class="habilidade-7">
-					<h2>
-						<i class="fa fa-wordpress"></i> <strong>Wordpress</strong> // 70%
-						<div class="barra"><span></span></div>
-					</h2>
-				</li>
-				<li class="habilidade-9">
-					<h2>
-						<i class="fa fa-git"></i> <strong>Git</strong> // 30%
-						<div class="barra"><span></span></div>
-					</h2>
-				</li>
-			</ul>
+
+				<?php endwhile; ?>
+
+				<?php else : ?>
+					Não há habilidades cadastradas...
+
+				<?php endif; ?>
+
+				</ul>
+
+			<?php wp_reset_query(); ?>
 
 
 			<a href="#" class="botao medio"><i class="fa fa-picture-o"></i> Acessar Portfolio</a>
